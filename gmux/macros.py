ff79fe10e793c12334df46c43b8f4cc791fe900f
Sam Lader <sam@samlader.com>
2024-04-21 00:11:52 +0100

    Add LRU cache

--- gmux/macros.py
+++ gmux/macros.py
@@ -1,8 +1,10 @@
 import ollama
+from functools import lru_cache
 
 
+@lru_cache(maxsize=None)
 def ollama_chat(model_name, prompt):
-    # TODO: Some prompt engineering here and add additional context
+    # TODO: Some prompt engineering here and add additional context (e.g. diff files)
     response = ollama.chat(
         model=model_name,
         messages=[
